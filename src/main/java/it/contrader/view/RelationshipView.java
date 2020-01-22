6ee6b8319935181f4f754d502257c1f57659efc7
Cocco0 <gemellidiversi_96@alice.it>
2020-01-22 12:53:08 +0100

    Created Relationship View

---
+++ src/main/java/it/contrader/view/RelationshipView.java
@@ -0,0 +1,68 @@
+package it.contrader.view;
+
+import java.util.List;
+
+import it.contrader.controller.Request;
+import it.contrader.dto.RelationshipDTO;
+import it.contrader.main.MainDispatcher;
+
+
+/**
+ * 
+ * @author Salvatore
+ *
+ * Si osservi che alla View arrivano solo oggetti di tipo DTO!
+ */
+public class RelationshipView extends AbstractView {
+
+	private Request request;
+	private String choice;
+
+	public RelationshipView() {
+		
+	}
+
+	/**
+	 * Mostra la lista 
+	 */
+	@Override
+	public void showResults(Request request) {
+		if (request != null) {
+			System.out.println("\n------------------- Gestione relazioni tra entità ----------------\n");
+			System.out.println("ID\tEntity1\tEntity2\t");
+			System.out.println("----------------------------------------------------\n");
+			
+			@SuppressWarnings("unchecked")
+			List<RelationshipDTO> relationships = (List<RelationshipDTO>) request.get("relationships");
+			for (RelationshipDTO u: relationships)
+				System.out.println(u);
+			System.out.println();
+		}
+	}
+
+	/**
+	 * Chiede all'utente un input (lettera da tastiera) per la choice (vedi RelationshipController). 
+	 * Mette la modalità GETCHOICE nella mode.
+	 */
+	@Override
+	public void showOptions() {
+		System.out.println("          Scegli l'operazione da effettuare:");
+		System.out.println("[L]eggi [I]nserisci [M]odifica [C]ancella [B]ack [E]sci");
+
+		this.choice = getInput();
+
+		
+	}
+	
+	/**
+	 * Impacchetta la request e la manda al RelationshipController.
+	 */
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("choice", choice);
+		request.put("mode", "GETCHOICE");
+		MainDispatcher.getInstance().callAction("User", "doControl", this.request);
+	}
+
+}

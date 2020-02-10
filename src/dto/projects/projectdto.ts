d0b986545494e96d5e42dd6ca2bf2ffa3bc7d0a1
Cocco0 <gemellidiversi_96@alice.it>
2020-02-10 17:08:50 +0100

    Updated Project and components files

---
+++ src/dto/projects/projectdto.ts
@@ -0,0 +1,21 @@
+
+/**
+ * Classe DTO di User. DEVE essere uguale (stesso nome classe, stessi attributi e stessi nomi) a
+ * quello nel backend. 
+ * 
+ * @see Usertype
+ * 
+ * @author Vittorio Valent
+ */
+export class ProjectDTO {
+
+   id: number;
+
+   name: string;
+
+   description: string;
+
+   shippingdate: string;
+
+}
+

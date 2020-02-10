41c2855e52f4a9d9db971833f9a0189042127cc9
Selenia88 <selenia.brunco6@gmail.com>
2020-02-10 15:02:08 +0100

    entityowner and entitycustomer

---
+++ src/main/java/it/contrader/dto/EntityOwnerDTO.java
@@ -0,0 +1,33 @@
+package it.contrader.dto;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import it.contrader.model.EntityOwner;
+import it.contrader.model.Project;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO della classe User. Ha gli stessi attributi di User
+ * 
+ * @author Vittorio Valent & Girolamo Murdaca
+ * 
+ *@see User
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+public class EntityOwnerDTO {
+
+	private long id;
+	
+	private String name;
+	
+	private Project project;
+
+	
+	
+
+}

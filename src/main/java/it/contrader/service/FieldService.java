83d96cdd473f92458a5c604b9dcf5345b9930fd3
Depy_96 <giuseppe.de.palma.gdp@gmail.com>
2020-02-10 15:43:41 +0100

    update

--- src/main/java/it/contrader/service/FieldService.java
+++ src/main/java/it/contrader/service/FieldService.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Service;
 import it.contrader.dao.EntityOwnerRepository;
 import it.contrader.dao.FieldRepository;
 import it.contrader.dto.FieldDTO;
+import it.contrader.model.EntityOwner;
 import it.contrader.model.Field;
 
 @Service
 public class FieldService extends AbstractService<Field,FieldDTO> {
 	
 	public FieldDTO findByEntityowner(EntityOwner  entityowner) {
-		return converter.toDTO(((FieldRepository)repository).findByEntityowner(EntityOwner  entityowner));
+		return converter.toDTO(((FieldRepository)repository).findByEntityowner( entityowner));
 	}
 	
 	public FieldDTO findByEntityownerAndNameAndTypeAndLenght(EntityOwner entityowner, String name, String type, Long lenght) {

64ad0b144106a6c8f164dfa98aef665eefe0c7a1
Selenia88 <selenia.brunco6@gmail.com>
2020-02-10 16:03:59 +0100

    controller

--- src/main/java/it/contrader/controller/EntityCustomerController.java
+++ src/main/java/it/contrader/controller/EntityCustomerController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import it.contrader.dto.EntityCustomerDTO;
 import it.contrader.service.EntityCustomerService;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/entityCustomer")
 public class EntityCustomerController {
 
--- src/main/java/it/contrader/controller/EntityOwnerController.java
+++ src/main/java/it/contrader/controller/EntityOwnerController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import it.contrader.dto.EntityCustomerDTO;
 
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/entityowner")
 public class EntityOwnerController {
 

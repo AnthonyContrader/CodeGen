2e2323d1917f74a3cd6453a5ddb105ba6249165d
Selenia88 <selenia.brunco6@gmail.com>
2020-02-11 14:06:41 +0100

    entitydto

--- src/dto/entitydto.ts
+++ src/dto/entitydto.ts
@@ -1,6 +1,7 @@
 /**
  * @author Selenia Brunco
  */
+import { ProjectDTO } from 'src/dto/projectdto';
 export class EntityOwnerDTO {
 
     
@@ -9,7 +10,7 @@ export class EntityOwnerDTO {
  
     name: string;
 
-    project: project;
+    project: ProjectDTO;
  
     
      

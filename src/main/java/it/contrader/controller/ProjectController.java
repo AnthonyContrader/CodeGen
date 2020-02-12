35c11a6c23d4cf35a497c518dfbbb3709d4c6656
Cocco0 <gemellidiversi_96@alice.it>
2020-02-12 09:49:52 +0100

    Fixed Rest controller of Project and Relationship

--- src/main/java/it/contrader/controller/ProjectController.java
+++ src/main/java/it/contrader/controller/ProjectController.java
@@ -1,11 +1,11 @@
 package it.contrader.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import it.contrader.dto.ProjectDTO;
 
-@Controller
+@RestController
 @RequestMapping("/project")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ProjectController extends AbstractController<ProjectDTO>{
--- src/main/java/it/contrader/controller/RelationshipController.java
+++ src/main/java/it/contrader/controller/RelationshipController.java
@@ -1,121 +1,13 @@
 package it.contrader.controller;
 
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import it.contrader.dto.RelationshipDTO;
-import it.contrader.model.EntityCustomer;
-import it.contrader.model.EntityOwner;
-import it.contrader.dto.LogDTO;
-import it.contrader.service.EntityOwnerService;
-import it.contrader.service.RelationshipService;
-import it.contrader.service.EntityCustomerService;
-import it.contrader.service.LogService;
 
-@Controller
+@RestController
 @RequestMapping("/relationship")
 @CrossOrigin(origins = "http://localhost:4200")
-public class RelationshipController {
-
-	@Autowired
-	private RelationshipService service;
-	
-	@Autowired
-	private EntityOwnerService serviceEntityo;
-	
-	@Autowired
-	private EntityCustomerService serviceEntityc;
-	
-	@Autowired
-	private LogService serviceLog;
-
-	
-	@GetMapping("/getall")
-	public String getAll(HttpServletRequest request) {
-		setAll(request);
-		InsertLOG(request,"Show Relationship"); 
-		return "relationships";
-	}
-
-	@GetMapping("/delete")
-	public String delete(HttpServletRequest request, @RequestParam("id") Long id) {
-		service.delete(id);
-		setAll(request);
-		InsertLOG(request,"Delete Relationship"); 
-		return "relationships";
-	}
-
-	@GetMapping("/preupdate")
-	public String preUpdate(HttpServletRequest request, @RequestParam("id") Long id) {
-		request.getSession().setAttribute("dto", service.read(id));
-		return "updaterelationship";
-	}
-
-	@PostMapping("/update")
-	public String update(HttpServletRequest request, @RequestParam("id") Long id, @RequestParam("entityowner") EntityOwner entityowner,
-			@RequestParam("entitycustomer") EntityCustomer entitycustomer) {
-
-		RelationshipDTO dto = new RelationshipDTO();
-		dto.setId(id);
-		dto.setEntityowner(entityowner);
-		dto.setEntitycustomer(entitycustomer);
-		service.update(dto);
-		setAll(request);
-		InsertLOG(request,"Update Relationship");
-		return "relationships";
-
-	}
-
-	@PostMapping("/insert")
-	public String insert(HttpServletRequest request, @RequestParam("entityowner") EntityOwner entityowner,
-			@RequestParam("entitycustomer") EntityCustomer entitycustomer) {
-		RelationshipDTO dto = new RelationshipDTO();
-		dto.setEntityowner(entityowner);
-		dto.setEntitycustomer(entitycustomer);
-		service.insert(dto);
-		setAll(request);
-		InsertLOG(request,"Insert Relationship"); 
-		return "relationships";
-	}
-
-	@GetMapping("/read")
-	public String read(HttpServletRequest request, @RequestParam("id") Long id) {
-		request.getSession().setAttribute("dto", service.read(id));
-		InsertLOG(request,"Read Relationship"); 
-		return "readrelationship";
-	}
-
-	@GetMapping("/logout")
-	public String logout(HttpServletRequest request) {
-		request.getSession().invalidate();
-		return "index";
-	}
-
-	private void setAll(HttpServletRequest request) {
-		request.getSession().setAttribute("list", service.getAll());
-		request.getSession().setAttribute("listEntityo", serviceEntityo.getAll());
-		request.getSession().setAttribute("listEntityc", serviceEntityc.getAll());
-	}
-	
-	public void InsertLOG(HttpServletRequest request,String mode) {
-		String temp = ""+request.getSession().getAttribute("user");				
-		temp = (""+request.getSession().getAttribute("user")).replace("UserDTO(","").replace(")", "");
-		String[] User_temp = temp.split(",");
-		
-		String USER=User_temp[1].replace("username=","");
-		Date date = new Date();
-		
-		
-		serviceLog.insert(new LogDTO(USER,mode,date));
-	}
+public class RelationshipController extends AbstractController<RelationshipDTO> {
 
 }

126d0632de7588ff867e9067b1e4985b1c755336
Cocco0 <gemellidiversi_96@alice.it>
2020-02-12 18:00:56 +0100

    Updated Entity Controller

--- src/main/java/it/contrader/controller/EntityOwnerController.java
+++ src/main/java/it/contrader/controller/EntityOwnerController.java
@@ -1,24 +1,16 @@
 package it.contrader.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import it.contrader.dto.EntityOwnerDTO;
 
-import it.contrader.service.EntityOwnerService;
-
 
 @RestController
-@RequestMapping("/entityowner")
+@RequestMapping("/entity")
 @CrossOrigin(origins = "http://localhost:4200")
 public class EntityOwnerController extends AbstractController<EntityOwnerDTO>{
-
-	@Autowired
-	private EntityOwnerService entityOwnerService;
 	
 }
 

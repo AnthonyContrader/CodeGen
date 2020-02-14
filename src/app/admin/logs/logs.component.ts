cfa84acca7402ce329058ecccea59b358a9c78a5
Depy_96 <giuseppe.de.palma.gdp@gmail.com>
2020-02-14 15:26:06 +0100

    UPdate

--- src/app/admin/logs/logs.component.ts
+++ src/app/admin/logs/logs.component.ts
@@ -12,4 +12,15 @@ export class LogsComponent implements OnInit {
   ngOnInit() {
   }
 
+  getLogs() {
+    this.service.getAll().subscribe(fields => this.logs = logs);
+  }
+
+  insert(log: LogDTO){
+    this.service.insert(field).subscribe(() => this.getLogs());
+    this.logtoinsert = new LogDTO();
+  }
+  clear(log: LogDTO){
+    this.logtoinsert = new LogDTO();
+  }
 }

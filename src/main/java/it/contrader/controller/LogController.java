310c26b65970da577f92f1e0a7849ee0237b4898
Cocco0 <gemellidiversi_96@alice.it>
2020-02-14 17:58:24 +0100

    Fixed log controller

--- src/main/java/it/contrader/controller/LogController.java
+++ src/main/java/it/contrader/controller/LogController.java
@@ -1,26 +1,14 @@
 package it.contrader.controller;
 
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import it.contrader.dto.LogDTO;
-import it.contrader.service.LogService;
-import it.contrader.service.UserService;
 
 @RestController
 @RequestMapping("/log")
 @CrossOrigin(origins = "http://localhost:4200")
-public class LogController {
+public class LogController extends AbstractController<LogDTO> {
 
 
 }

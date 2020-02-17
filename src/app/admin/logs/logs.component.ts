2b6faba7f99ed92f38cac50e96149c4e6a591240
Depy_96 <giuseppe.de.palma.gdp@gmail.com>
2020-02-17 17:46:31 +0100

    Update

--- src/app/admin/logs/logs.component.ts
+++ src/app/admin/logs/logs.component.ts
@@ -20,12 +20,28 @@ export class LogsComponent implements OnInit {
     this.getLogs();
     
   }
+  FormatDate()
+  {
+    let i:number=0;
+    while(document.getElementById(i.toString()) != null)
+    {
+      var temp = (<HTMLInputElement>document.getElementById(i.toString())).value;
+      var data = temp.substring(0, 10);
+      var ora = temp.substring(11, 19);
+      var date = data.split("-");
+      var data=date[2]+"/"+date[1]+"/"+date[0];
 
-  onLoad(){
-    FormatDate();
+      //alert(data+" "+ora);
+     // $("#"+i).val(data+" "+ora);
+
+      //(<HTMLInputElement>document.getElementById(i.toString())).setAttribute("value",data+" "+ora);
+      i++;
+    }
   }
   getLogs() {
+    
     this.service.getAll().subscribe(logs => this.logs = logs);
+    FormatDate();
   }
   
   insert(log: LogDTO){

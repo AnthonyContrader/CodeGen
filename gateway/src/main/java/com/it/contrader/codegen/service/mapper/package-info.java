9b1ede74f5bf8041a36f8f5ec24cd5b306ae1bd7
Selenia88 <selenia.brunco6@gmail.com>
2020-02-25 15:36:05 +0100

    project empty

--- gateway/src/main/java/com/it/contrader/codegen/service/mapper/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * MapStruct mappers for mapping domain objects and Data Transfer Objects.
- */
-package com.it.contrader.codegen.service.mapper;
--- src/main/java/com/it/contrader/codegen/config/audit/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Audit specific code.
- */
-package com.it.contrader.codegen.config.audit;
